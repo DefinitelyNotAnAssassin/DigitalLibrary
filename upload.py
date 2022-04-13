628be64fabb7b8f37717889823879ba074ffd378
DefinitelyNotAnAssassin <dharklike@gmail.com>
2022-04-13 12:08:59 +0800

    Continue to laptop

--- upload.py
+++ upload.py
@@ -4,16 +4,24 @@ import sqlite3
 import os
 from sqlalchemy import and_, or_
 from flask_sqlalchemy import SQLAlchemy
-
+import uuid
+import flask_bcrypt
 app = Flask(__name__)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///library.sqlite3'
 
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["FILE_UPLOAD"] = f"{os.getcwd()}/files/"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///library.sqlite3"
+app.config["SQLALCHEMY_BINDS"] = {
+    'admin':        'sqlite:///admin.sqlite3',
+    'libraryfiles':      'sqlite:///library.sqlite3'
+}
+
 db = SQLAlchemy(app)
 
 
 class libraryfiles(db.Model):
+  __bind_key__ = "libraryfiles"
   _id = db.Column("id", db.Integer, primary_key = True)
   bookname = db.Column("bookname", db.String(100))
   filename = db.Column("filename", db.String(255))
@@ -26,6 +34,7 @@ class libraryfiles(db.Model):
     self.category = category
     
 class messages(db.Model):
+  __bind_key__ = "libraryfiles"
   _id = db.Column("id", db.Integer, primary_key= True)
   bookname = db.Column("bookname", db.String(100))
   content = db.Column("content", db.String(1000))
@@ -39,9 +48,20 @@ class messages(db.Model):
     self.date = date
     self.sender = sender
     self.rating = rating
+class user(db.Model):
+  __bind_key__ = "admin"
+  _id = db.Column("id", db.Integer, primary_key = True)
+  username = db.Column("username", db.String(100))
+  password =db.Column("password", db.String(255))
+class access(db.Model):
+  _id = db.Column("id", db.Integer, primary_key = True)
+  key = db.Column("key",db.String(100))
+  
+
 
 
 def verification(filename):
+  __bind_key__ = "admin"
   decision = input(f"A user is trying to upload {filename} Accept(Y) or Reject(N) : ")
   return decision 
 
@@ -67,7 +87,14 @@ def upload():
     return redirect(url_for("add_content"))
   elif decision.lower() == "n":
     return "<h1>File Upload Request Rejected</h1>"
-    
+
+
+@app.route("/register")
+def register():
+  return render_template("register.html")
+
+
   
 if __name__ == "__main__":
   app.run(debug = True)
+  #db.create_all(bind = ['admin'])
